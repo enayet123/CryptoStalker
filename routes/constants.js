92a358238486277f7dfcc111fa372e7f9d7fdede
Enayet Hussain <enayet123@hotmail.com>
2021-09-04 11:28:29 +0100

    WIP

--- routes/constants.js
+++ routes/constants.js
@@ -1,4 +1,5 @@
 module.exports = Object.freeze({
+  COIN_FILE: 'coins.yml',
   MSG_TYPE: '0',
   HEARTBEAT_TYPE: '999',
   STREAM_OPEN: 'open',
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const constants = require('./constants');
 const cache = require('memory-cache');
 const CryptoCompare = require('./cryptocompare');
 const env = require('dotenv');
+const yaml = require('yaml');
+const fs = require('fs');
 
 env.config();
 
@@ -13,65 +15,72 @@ const envKeys = ["PORT", "VERBOSE", "DEBUG", "EXCHANGE_HTTPS", "EXCHANGE_PARAMS"
 console.log("[ENVIR]");
 envKeys.forEach(key => console.log("  ", key, process.env[key]));
 
+// Create coins.yml file if not exists
+if (!fs.existsSync(constants.COIN_FILE))
+  fs.open(constants.COIN_FILE, 'w', (err) => {
+    if (err) throw err;
+    console.log('[FILE]', 'Created new ' + constants.COIN_FILE + ' file')
+  })
+
+console.log(process.env.COINS)
 // Obj used to store current prices, acting as a sort of dictionary
 
-const store = {};
-const coins = JSON.parse(process.env.COINS);
-const subs = util.getSubscriptions(coins);
-let stream = CryptoCompare.connect().withReconnect().subscribe(subs).addTradeListener(store);
-// let stream = cryptocompare.connect(subs);
-// cryptocompare.registerStream({ stream, store });
-util.getConversion();
+// const store = {};
+// const coins = JSON.parse(process.env.COINS);
+// const subs = util.getSubscriptions(coins);
+// let stream = CryptoCompare.connect().withReconnect().subscribe(subs).addTradeListener(store);
+// // cryptocompare.registerStream({ stream, store });
+// util.getConversion();
 
-const update24 = () => {
-  const stored = { ...store };
+// const update24 = () => {
+//   const stored = { ...store };
 
-  for (const coinName in stored) {
-    cache.put(`${coinName}24`, JSON.stringify(stored[coinName]));
-  }
-}
+//   for (const coinName in stored) {
+//     cache.put(`${coinName}24`, JSON.stringify(stored[coinName]));
+//   }
+// }
 
-const update = (forced, res) => {
-  let changes = [];
-  const cached = JSON.parse(cache.get(constants.STORE));
+// const update = (forced, res) => {
+//   let changes = [];
+//   const cached = JSON.parse(cache.get(constants.STORE));
 
-  for (const coinName in cached) {
-    const storeData = { ...store }[coinName];
-    const cacheData = JSON.parse(cache.get(coinName));
-    const cache24Data = JSON.parse(cache.get(`${coinName}24`));
+//   for (const coinName in cached) {
+//     const storeData = { ...store }[coinName];
+//     const cacheData = JSON.parse(cache.get(coinName));
+//     const cache24Data = JSON.parse(cache.get(`${coinName}24`));
     
-    if (cache24Data && cacheData) {
-      const margin = coins[coinName].margin;
-      const difference = Math.abs(storeData.USD - cacheData.USD);
+//     if (cache24Data && cacheData) {
+//       const margin = coins[coinName].margin;
+//       const difference = Math.abs(storeData.USD - cacheData.USD);
       
-      if (difference > margin || forced) {
-        const emoji = coins[coinName].emoji;
-        const { delta, dailyMovement, arrowEmoji, arrow24Emoji } = util.getStats(cache24Data, cacheData, storeData);
-        cache.put(coinName, JSON.stringify(storeData));
-        changes = [ ...changes, `${emoji} ${arrowEmoji} $${delta} → ${util.asUSD(storeData.USD)} ≅ ${util.asGBP(storeData.USD)} ${arrow24Emoji} ${dailyMovement}%`];
-      }
-    } else {
-      cache.put(coinName, JSON.stringify(storeData));
-    }
-  }
+//       if (difference > margin || forced) {
+//         const emoji = coins[coinName].emoji;
+//         const { delta, dailyMovement, arrowEmoji, arrow24Emoji } = util.getStats(cache24Data, cacheData, storeData);
+//         cache.put(coinName, JSON.stringify(storeData));
+//         changes = [ ...changes, `${emoji} ${arrowEmoji} $${delta} → ${util.asUSD(storeData.USD)} ≅ ${util.asGBP(storeData.USD)} ${arrow24Emoji} ${dailyMovement}%`];
+//       }
+//     } else {
+//       cache.put(coinName, JSON.stringify(storeData));
+//     }
+//   }
 
-  if (changes.length) {
-    const message = changes.reduce((acc, val) => acc + '\n' + val);
-    util.sendToSlack(message);
-    if (forced) res.status(204).send();
-  }
-};
+//   if (changes.length) {
+//     const message = changes.reduce((acc, val) => acc + '\n' + val);
+//     util.sendToSlack(message);
+//     if (forced) res.status(204).send();
+//   }
+// };
 
-setTimeout(() => update24(), 30000);
+// setTimeout(() => update24(), 30000);
 
-router.get('/', (_, res) => res.json(coins));
+// router.get('/', (_, res) => res.json(coins));
 
-router.all('/now', (_, res)  => update(true, res));
+// router.all('/now', (_, res)  => update(true, res));
 
-cron.schedule('* * * * *', () => update(false));
+// cron.schedule('* * * * *', () => update(false));
 
-cron.schedule('0 0 * * *', () => update24());
+// cron.schedule('0 0 * * *', () => update24());
 
-cron.schedule('0 0 * * *', () => util.getConversion());
+// cron.schedule('0 0 * * *', () => util.getConversion());
 
 module.exports = router;
